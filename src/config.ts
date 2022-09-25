a8442dd580f9137dd694ca6834979e04426a776b
Prince-Gizard <buraevvasilii@gmail.com>
2022-09-25 19:44:51 +0300

    _

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'What I Use'
+export const SITE_DESCRIPTION = 'Тут могло быть ваше описание'
+export const HOMEPAGE_URL = 'https://vasilii-buraev.vercel.app/'
